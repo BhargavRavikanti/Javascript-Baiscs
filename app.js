04d14475e88aae126b0be5284b49a9d3918f8629
BhargavRavikanti20 <bhargavravikanti20@gmail.com>
2023-09-01 13:41:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5582,10 +5582,354 @@ NOTES
 // const parityList3 = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
 // console.log(parityList3);
 
-// 006
+// 006 Array.find
 // ======================================
-// 007
+// FIND returns the value of the FIRST ELEMENT in the array that satisfies the provived testing function.
+
+/*
+// NOTES
+
+// let movies = [
+//   "The Fantastic Mr. Fox",
+//   "Mr. and Mrs. Smith",
+//   "Mrs. Doubtfire",
+//   "Mr. Deeds",
+// ];
+
+// // Find first movie that includes 'Mrs' in its title:
+// const movie = movies.find((movie) => {
+//   return movie.includes("Mrs");
+// });
+
+// //Find first movie that begins with 'Mrs':
+// const movie2 = movies.find((m) => m.indexOf("Mrs") === 0);
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//   },
+// ];
+// //Find first book w/ rating over 4.3
+// const goodBook = books.find((b) => b.rating >= 4.3);
+
+// //Find first book with Neil Gaiman as an author:
+// const neilBook = books.find((b) => b.authors.includes("Neil Gaiman"));
+*/
+
+// My Practice
+
+// let movies = [
+//   "The Fantastic Mr. Fox",
+//   "Mr. and Mrs. Smith",
+//   "Mrs. Doubtfire",
+//   "Mr. Deeds",
+// ];
+
+// const movie1 = movies.find((m) => {
+//   return m.includes("Mrs");
+// });
+// console.log(movie1);
+
+// const movie2 = movies.find((m) => {
+//   return m.indexOf("Mrs") === 0;
+// });
+// console.log(movie2);
+
+// const movie3 = movies.find((m) => m.indexOf("Mrs") === 0);
+// console.log(movie3);
+
+// Another Example
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//   },
+// ];
+
+// const goodBook = books.find((b) => {
+//   return b.rating >= 4.3;
+// });
+// console.log(goodBook);
+
+// const neilBook = books.find((b) => b.authors.includes("Neil Gaiman"));
+// console.log(neilBook); // find only gives first occurence
+
+// const goodBook2 = books.find((b) => {
+//   return b.rating === 4.11 && b.authors.includes("Neil Gaiman");
+// });
+// console.log(goodBook2);
+
+// 007 Filter
 // ======================================
+// FILTER creates a new array with all elements that pass the test implemented by the provided function.
+// Javascript Array filter(). The filter() method returns a new array with all elements that pass the test defined by the given function.
+
+/*
+// NOTES
+
+// const nums = [34, 35, 67, 54, 109, 102, 32, 9];
+
+// const odds = nums.filter((n) => n % 2 === 1);
+// const evens = nums.filter((n) => n % 2 === 0);
+// const bigNums = nums.filter((n) => n > 50);
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "Changing My Mind",
+//     authors: ["Zadie Smith"],
+//     rating: 3.83,
+//     genres: ["nonfiction", "essays"],
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//     genres: ["fiction", "graphic novel", "fantasy"],
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//     genres: ["fiction", "historical fiction"],
+//   },
+//   {
+//     title: "The Name of the Wind",
+//     authors: ["Patrick Rothfuss"],
+//     rating: 4.54,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "The Overstory",
+//     authors: ["Richard Powers"],
+//     rating: 4.19,
+//     genres: ["fiction", "short stories"],
+//   },
+//   {
+//     title: "The Way of Kings",
+//     authors: ["Brandon Sanderson"],
+//     rating: 4.65,
+//     genres: ["fantasy", "epic"],
+//   },
+//   {
+//     title: "Lord of the flies",
+//     authors: ["William Golding"],
+//     rating: 3.67,
+//     genres: ["fiction"],
+//   },
+// ];
+
+// //All books rated over 4.3:
+// const goodBooks = books.filter((b) => b.rating > 4.3);
+
+// // All books with fantasy as a genre:
+// const fantasyBooks = books.filter((book) => book.genres.includes("fantasy"));
+
+// // all books that are either short stories or essays:
+// const shortForm = books.filter(
+//   (book) =>
+//     book.genres.includes("short stories") || book.genres.includes("essays")
+// );
+
+// // Simple search functionality:
+// const query = "The";
+// const results = books.filter((book) => {
+//   const title = book.title.toLowerCase();
+//   return title.includes(query.toLowerCase());
+// });
+*/
+
+// My Practice
+
+// example 1
+
+// const nums = [34, 35, 67, 54, 109, 102, 32, 9];
+
+// const odds = nums.filter((n) => n % 2 === 1);
+// console.log(odds);
+
+// const evens = nums.filter((n) => n % 2 === 0);
+// console.log(evens);
+// console.log(nums); // original will not change
+
+// const bigNums = nums.filter((n) => n > 50);
+// console.log(bigNums);
+
+// const smallNums = nums.filter((n) => n < 50);
+// console.log(smallNums);
+
+// Another Example 2
+// FILTER creates a new array with all elements that pass the test implemented by the provided function.
+// Javascript Array filter(). The filter() method returns a new array with all elements that pass the test defined by the given function.
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "Changing My Mind",
+//     authors: ["Zadie Smith"],
+//     rating: 3.83,
+//     genres: ["nonfiction", "essays"],
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//     genres: ["fiction", "graphic novel", "fantasy"],
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//     genres: ["fiction", "historical fiction"],
+//   },
+//   {
+//     title: "The Name of the Wind",
+//     authors: ["Patrick Rothfuss"],
+//     rating: 4.54,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "The Overstory",
+//     authors: ["Richard Powers"],
+//     rating: 4.19,
+//     genres: ["fiction", "short stories"],
+//   },
+//   {
+//     title: "The Way of Kings",
+//     authors: ["Brandon Sanderson"],
+//     rating: 4.65,
+//     genres: ["fantasy", "epic"],
+//   },
+//   {
+//     title: "Lord of the flies",
+//     authors: ["William Golding"],
+//     rating: 3.67,
+//     genres: ["fiction"],
+//   },
+// ];
+
+// const goodBooks = books.filter((b) => b.rating > 4.3);
+// console.log(goodBooks);
+// console.log(books); // original will not change
+
+// const fantasyBooks = books.filter((b) => b.genres.includes("fantasy"));
+// console.log(fantasyBooks);
+
+// const epic = books.filter((b) => b.genres.includes("epic")); // one-liner arrow function
+// console.log(epic);
+// // OR
+// const epicBook = books.filter((b) => {
+//   return b.genres.includes("epic");
+// });
+// console.log(epicBook);
+
+// // one-liner arrow function
+// const graphicNovelBook = books.filter(
+//   (b) => b.genres.includes("graphic novel") // case sensitive if you use capital N gives empty array
+// );
+// console.log(graphicNovelBook);
+// // OR using return
+// const graphicNovelBook1 = books.filter((b) => {
+//   return b.genres.includes("graphic novel");
+// });
+// console.log(graphicNovelBook1);
+
+// const shortStoryEssays = books.filter(
+//   (b) => b.genres.includes("short stories") || b.genres.includes("essays")
+// );
+// console.log(shortStoryEssays);
+
+// const searchThe = books.filter((b) => b.title.includes("The"));
+// console.log(searchThe); // only gets books that has The
+
+// // IMPORTANT METHOD TO REMEMBER
+// // SEARCH METHOD STEPS  does not matter if the search enter value is case sensitive
+// const query = "The";
+// const results = books.filter((b) => {
+//   const title = b.title.toLowerCase(); // converting title to lowercase
+//   return title.includes(query.toLowerCase()); // query to lowercase
+// });
+// console.log(results);
+
+// Another Example 3
+
+// const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const odds = nums.filter((n) => {
+//   return n % 2 === 1; // our callback returns true or false
+//   // if it returns true, n is added to the filtered array
+// });
+// console.log(odds); // (5) [9, 7, 5, 3, 1]
+// // one-liner arrow function
+// const odds1 = nums.filter((n) => n % 2 === 1);
+// console.log(odds1);
+
+// const smallNums = nums.filter((n) => {
+//   return n < 5;
+// });
+// console.log(smallNums);
+// // one-liner arrow function
+// const smallNums1 = nums.filter((n) => n < 5);
+// console.log(smallNums1);
+
 // 008
 // ======================================
+// 009
+// ======================================
+// 010
 // ======================================
