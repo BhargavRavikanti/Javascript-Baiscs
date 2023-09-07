509341531f115f52d2712ab7390750fa531fe63b
BhargavRavikanti20 <bhargavravikanti20@gmail.com>
2023-09-07 23:33:17 +0530

    learning callback hell

--- app.js
+++ app.js
@@ -10514,6 +10514,144 @@ console.log('I HAPPEN SECOND!');
 /*
 // NOTES
 
+const btn = document.querySelector('button');
+
+//This function moves an element "amount" number of pixels after a delay.
+//If the element will stay on screen, we move the element and call the onSuccess callback function
+//If the element will move off screen, we do not move the element and instead call the onFailure callback
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+	setTimeout(() => {
+		const bodyBoundary = document.body.clientWidth;
+		const elRight = element.getBoundingClientRect().right;
+		const currLeft = element.getBoundingClientRect().left;
+		if (elRight + amount > bodyBoundary) {
+			onFailure();
+		}
+		else {
+			element.style.transform = `translateX(${currLeft + amount}px)`;
+			onSuccess();
+		}
+	}, delay);
+};
+
+// LOOK AT THIS UGLY MESS!
+moveX(
+	btn,
+	300,
+	1000,
+	() => {
+		//success callback
+		moveX(
+			btn,
+			300,
+			1000,
+			() => {
+				//success callback
+				moveX(
+					btn,
+					300,
+					1000,
+					() => {
+						//success callback
+						moveX(
+							btn,
+							300,
+							1000,
+							() => {
+								//success callback
+								moveX(
+									btn,
+									300,
+									1000,
+									() => {
+										//success callback
+										alert('YOU HAVE A WIDE SCREEN!');
+									},
+									() => {
+										//failure callback
+										alert('CANNOT MOVE FURTHER!');
+									}
+								);
+							},
+							() => {
+								//failure callback
+								alert('CANNOT MOVE FURTHER!');
+							}
+						);
+					},
+					() => {
+						//failure callback
+						alert('CANNOT MOVE FURTHER!');
+					}
+				);
+			},
+			() => {
+				//failure callback
+				alert('CANNOT MOVE FURTHER!');
+			}
+		);
+	},
+	() => {
+		//failure callback
+		alert('CANNOT MOVE FURTHER!');
+	}
+);
+
+*/
+
+// My Practice
+
+const btn = document.querySelector("button");
+
+// setTimeout(() => {
+//   btn.style.transform = `translate(100px)`;
+//   setTimeout(() => {
+//     btn.style.transform = `translate(200px)`;
+//     setTimeout(() => {
+//       btn.style.transform = `translate(300px)`;
+//       setTimeout(() => {
+//         btn.style.transform = `translate(400px)`;
+//         setTimeout(() => {
+//           btn.style.transform = `translate(500px)`;
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const moveX = (element, amount, delay, callback) => {
+  setTimeout(() => {
+    element.style.transform = `translateX(${amount}px)`;
+    if (callback) callback();
+  }, delay);
+};
+moveX(btn, 100, 1000, () => {
+  moveX(btn, 200, 1000, () => {
+    moveX(btn, 300, 1000, () => {
+      moveX(btn, 400, 1000, () => {
+        moveX(btn, 500, 1000);
+      });
+    });
+  });
+});
+
+// ===================================
+// 006 -
+// ===================================
+
+/*
+// NOTES
+
+*/
+
+// My Practice
+// ===================================
+// 007 -
+// ===================================
+
+/*
+// NOTES
+
 */
 
 // My Practice
